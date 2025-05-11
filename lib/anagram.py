78f03f1ee686ed4455082b739a07ba8fb85bf302
NatalieGichuki <nataliegichuki@gmail.com>
2025-05-11 16:02:35 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [char for char in word_list if sorted(char.lower()) == sorted(self.word) and char.lower() != self.word]
+        
+    pass
